b9f23cce1241363f5a4f6b1ee908e937d5d81243
Zhentian Wan <zhentian.wan@varian.com>
2021-06-29 18:34:02 +0300

    finished

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '7grvtuwf6l'
+const apiId = 'ef36tbt11l'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
